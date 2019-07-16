fc05d0a9187e6a38dc5e0abc5a89659fbcee3915
limk-j <38344974+limk-j@users.noreply.github.com>
2019-07-16 14:09:28 +0800

    add history function

--- js/base.js
+++ js/base.js
@@ -1073,5 +1073,66 @@ return list;
 	})
 
 	$('#tohistory').on('click', function () {
-		$('.container').attr('style', 'visibility: visible').find('.pop-up').eq(3).attr('style', 'visibility: visible').siblings().attr('style', 'visibility: hidden');
+		$('.container').attr('style', 'visibility: visible').find('.pop-up').eq(0).attr('style', 'visibility: visible').siblings().attr('style', 'visibility: hidden');
 	})
+
+
+// 查询历史放大图表
+var historychat = echarts.init(document.getElementById('historychat'));
+function chart_history(){
+	historychat.clear();
+	option = {
+		title : {
+			text: '统计信息',
+			x:'center'
+		},
+		tooltip : {
+			trigger: 'item',
+			formatter: "{a} <br/>{b} : {c} ({d}%)"
+		},
+		legend: {
+			x : 'center',
+			y : 'bottom',
+			data:['查询','分析','预测']
+		},
+		toolbox: {
+			show : true,
+			feature : {
+				mark : {show: true},
+				magicType : {
+					show: true, 
+					type: ['pie', 'funnel']
+				},
+				restore : {show: true},
+				saveAsImage : {show: true}
+			}
+		},
+		calculable : true,
+		series : [
+			{
+				name:'占比',
+				type:'pie',
+				radius : [30, 110],
+				//center : ['75%', 200],
+				roseType : 'area',
+				//x: '50%',               // for funnel
+				sort : 'ascending',     // for funnel
+				data:[
+					{value:10, name:'查询'},
+					{value:5, name:'分析'},
+					{value:15, name:'预测'}
+				]
+			}
+		]
+	}; 
+	historychat.setOption(option);
+}
+chart_history();
+
+$('.savehistory').on('click',function(){
+	$(".table3").table2excel({
+    exclude: ".excludeThisClass",
+    name: "查询历史",
+    filename: "history" //do not include extension
+});
+})
